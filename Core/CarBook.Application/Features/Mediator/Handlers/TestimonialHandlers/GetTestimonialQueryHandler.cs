a8d615b3b42cb07c870cf7f275115da532edfa9a
Şuayb Hızar <76668077+Suaybhizar@users.noreply.github.com>
2023-12-18 23:16:05 +0300

    About page consumption operations and reference mediator and reference listing have been completed successfully. (Reference backend codes are written)

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/TestimonialHandlers/GetTestimonialQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using CarBook.Application.Features.Mediator.Queries.TestimonialQueries;
+using CarBook.Application.Features.Mediator.Results.TestimonialResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class GetTestimonialQueryHandler : IRequestHandler<GetTestimonialQuery, List<GetTestimonialQueryResult>>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public GetTestimonialQueryHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetTestimonialQueryResult>> Handle(GetTestimonialQuery request, CancellationToken cancellationToken)
+        {
+            var valuea = await _repository.GetAllAsync();
+            return valuea.Select(x => new GetTestimonialQueryResult
+            {
+                Comment = x.Comment,
+                ImageUrl = x.ImageUrl,
+                Name = x.Name,
+                TestimonialID = x.TestimonialID,
+                Title = x.Title,
+            }).ToList();
+        }
+    }
+}
+
