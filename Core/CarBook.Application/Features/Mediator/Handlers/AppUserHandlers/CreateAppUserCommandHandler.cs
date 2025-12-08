a6fe4f866be2b35d1851178ecae2316f8d9758d7
Şuayb Hızar <76668077+Suaybhizar@users.noreply.github.com>
2025-12-09 02:05:13 +0300

    Register and Login UI pages have been created successfully.

--- Core/CarBook.Application/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -26,7 +26,10 @@ namespace CarBook.Application.Features.Mediator.Handlers.AppUserHandlers
             {
                 UserName = request.UserName,
                 Password = request.Password,
-                AppRoleId = (int)RolesType.Member
+                AppRoleId = (int)RolesType.Member,
+                Email = request.Email,
+                Name = request.Name,
+                Surname = request.Surname
             });
         }
     }
---
+++ Frontends/CarBook.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Dto.LoginDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CreateLoginDto createLoginDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            return View();
+        }
+    }
+}
